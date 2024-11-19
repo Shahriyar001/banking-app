f6ddd6352d9985baeb7a758751140c9ec47f99f0
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-11-19 23:58:23 +0600

    added form

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -17,10 +17,19 @@ const AuthForm = ({ type }: { type: string }) => {
         <div className="flex flex-col gap-1 md:gap-3">
           <h1 className="text-24 lg:text-36 font-semibold text-gray-900">
             {user ? "Link Account" : type === "sign-in" ? "Sign In" : "Sign Up"}
-            <p className=""></p>
+            <p className="text-16 font-normal text-gray-600">
+              {user
+                ? "Link your account to get started"
+                : "place enter your details"}
+            </p>
           </h1>
         </div>
       </header>
+      {user ? (
+        <div className="flex flex-col gap-4">{/* plaidlink */}</div>
+      ) : (
+        <>Form</>
+      )}
     </section>
   );
 };

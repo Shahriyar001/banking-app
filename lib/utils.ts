151ce437245c78d25ca9f61f9b694971ca854b13
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-11-23 23:56:32 +0600

    complete auth ui

--- lib/utils.ts
+++ lib/utils.ts
@@ -201,8 +201,9 @@ export const authformSchema = (type: string) =>
     firstName: type === "sign-in" ? z.string().optional() : z.string().min(3),
     lastName: type === "sign-in" ? z.string().optional() : z.string().min(3),
     address1: type === "sign-in" ? z.string().optional() : z.string().max(50),
+    city: type === "sign-in" ? z.string().optional() : z.string().max(50),
     state:
-      type === "sign-in" ? z.string().optional() : z.string().min(2).max(2),
+      type === "sign-in" ? z.string().optional() : z.string().min(2).max(20),
     postalCOde:
       type === "sign-in" ? z.string().optional() : z.string().min(3).max(6),
     dateOfBirth: type === "sign-in" ? z.string().optional() : z.string().min(3),
